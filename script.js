52220d0d1c66dc489616e232a804080ac34f5ea3
Chris Rebuck <crebuck.apt@gmail.com>
2021-09-23 11:11:18 -0500

    implemented game logic as well as manipulating CSS styles if user successfully won the game.

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-"use strict";
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!🙌';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  //When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!!';
+
+    //When Player Wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!🙌';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Number is too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😓';
+      document.querySelector('.score').textContent = 0;
+    }
+    //When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = ' 📉 Number is too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😓';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
